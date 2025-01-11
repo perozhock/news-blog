958a15c7434a710f595a20b679947edd58c16961
perozhock <shat.prog1@gmail.com>
2025-01-11 21:59:08 +0300

    removed nested component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,67 +1,58 @@
-import { useCallback, useEffect, useState } from "react";
-import Modal from "./Modal";
+import React, { useCallback, useEffect, useState } from "react";
 import apiKey from "../api/apiKey";
 import axios from "axios";
+import NewsCard from "./NewsCard";
+import { Article } from "../types/article";
 
-const Content = () => {
-    const [count, setCount] = useState(0);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [data, setData] = useState();
-    const [country, setCountry] = useState("us");
+interface ContentProps {
+    category: string;
+}
+
+const Content: React.FC<ContentProps> = ({ category }) => {
+    const [data, setData] = useState<Article[]>();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [country, setCountry] = useState<string>("us");
 
     const getData = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+
         try {
             const response = await axios.get(
-                `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`
+                `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`
             );
-            setData(response.data);
+            setData(response.data.articles || []);
             console.log(response.data);
+        } catch (err) {
+            setError("Ошибка загрузки. Попробуйте позже");
+            console.error(err);
         } finally {
+            setLoading(false);
         }
-    }, []);
+    }, [category]);
 
     useEffect(() => {
-        let isMounted = true;
-        if (isMounted) {
-            getData();
-        }
-        return () => {
-            isMounted = false;
-        };
+        getData();
     }, [getData]);
 
-    return (
-        <div className="h-full flex items-center justify-center border-4 rounded-2xl">
-            <div className="flex flex-col border-4 rounded-2xl p-5">
-                <button
-                    className="inline-block text-4xl min-w-48 h-20 mb-4 border rounded-lg"
-                    onClick={() => setCount(count + 1)}
-                >
-                    count is {count}
-                </button>
-
-                <button
-                    className="inline-block border-2 rounded-lg align-middle min-w-12"
-                    onClick={() => setIsModalOpen(true)}
-                >
-                    +
-                </button>
-                <Modal
-                    isOpen={isModalOpen}
-                    onClose={() => setIsModalOpen(false)}
-                >
-                    <div className="flex">
-                        {data ? (
-                            <pre className="flex max-w-96 w-96 overflow-y-auto max-h-80">
-                                {JSON.stringify(data, null, 2)}
-                            </pre>
-                        ) : (
-                            <p>Загрузка данных...</p>
-                        )}
-                    </div>
-                    <input className="border-2 max-w-xs rounded-lg"></input>
-                </Modal>
+    if (loading)
+        return (
+            <div className="flex h-full items-center">
+                <p className="text-center text-5xl font-extrabold">Загрузка...</p>
             </div>
+        );
+    if (error) return (
+        <div className="flex h-full items-center">
+            <p className="text-center">{error}</p>
+        </div>
+    );
+
+    return (
+        <div className="flex flex-wrap h-full overflow-auto border shadow-md gap-6 rounded-2xl p-5">
+            {data?.map((article, index) => (
+                <NewsCard key={index} article={article} />
+            ))}
         </div>
     );
 };
