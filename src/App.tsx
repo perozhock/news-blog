08ba33201e4eb6bc7c696181bf9db91d3fb91273
perozhock <shat.prog1@gmail.com>
2025-01-11 21:57:20 +0300

    nested component moved to a higher-level component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import Content from "./components/Content";
+import Navigation from "./components/Navigation";
+import CategoryHeader from "./components/CategoryHeader";
+import { NavItems } from "./types/navItems";
+import { useState } from "react";
 
 function App() {
+    const [category, setCategory] = useState<string>("general");
+
     return (
         <div className="h-screen w-screen p-5">
-            <Content />
+            <div className="h-full flex flex-col items-center justify-start border shadow-md rounded-2xl">
+                <div className="flex min-h-14 items-center justify-between rounded-2xl border shadow-md w-full p-5">
+                    <CategoryHeader category={category} navItems={NavItems} />
+                    <Navigation category={category} setCategory={setCategory} />
+                </div>
+                <Content category={category}/>
+            </div>
         </div>
     );
 }
